94bc9108540c9476f08cf15d5b97bb939d918117
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-27 19:46:18 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,7 +19,8 @@ function frameIt() {
 
 function pressIt() {
   $('form #typing').on('keydown', function(key) {
-    if (key.which === )
-  })
-  
+    if (key.which === 71) {
+      alert ('g was pressed');
+    }
+  });
 }
